c1334b974a6fd2c246b3fe6530c29d67cef32da8
razzie13 <gregrasmussen@gmail.com>
2022-02-16 23:57:29 -0500

    Database Connections

--- plugins/wedding-rsvp-form/js/wedding-rsvp-form.js
+++ plugins/wedding-rsvp-form/js/wedding-rsvp-form.js
@@ -1,15 +1,11 @@
 const chooseToAttend = () => {
     console.log('function chooseToAttend')
-        document.getElementById('rsvp-attending').classList.remove('hide');
-        document.getElementById('rsvp-attending').classList.add('show');
-        document.getElementById('rsvp-not-attending').classList.remove('show');
-        document.getElementById('rsvp-not-attending').classList.add('hide');
+        document.getElementById('email-attend-input-section').classList.remove('hide');
+        document.getElementById('email-attend-input-section').classList.add('show');
 }
 
 const chooseNotToAttend = () => {
     console.log('function chooseNotToAttend')
-        document.getElementById('rsvp-not-attending').classList.remove('hide')
-        document.getElementById('rsvp-not-attending').classList.add('show');
-        document.getElementById('rsvp-attending').classList.remove('show');
-        document.getElementById('rsvp-attending').classList.add('hide');
+        document.getElementById('email-attend-input-section').classList.remove('show');
+        document.getElementById('email-attend-input-section').classList.add('hide');
 }
--- plugins/wedding-rsvp-form/wedding-rsvp-form.php
+++ plugins/wedding-rsvp-form/wedding-rsvp-form.php
@@ -11,18 +11,24 @@
  */
 
 require_once( $_SERVER['DOCUMENT_ROOT'] . '/wp-load.php' );
-$table_name = 'invite_responses';
-$column_name = 'postal_code';
-$result = $wpdb->get_results( "SELECT $column_name FROM $table_name");
 
-$form_postal_code = "";
-if(isset($_POST['submitbutton'])){ //check if form was submitted
-  $input = $_POST['postcode']; //get input text
-  $message = "Success! You entered: ".$input;
-}    
+global $wpdb;
+
+$searched_postcode = $_POST['postcode'];
+
+$query = "SELECT * FROM invite_responses WHERE Postal_Code = '{$searched_postcode}'";
+$result = $wpdb->get_row($query);
 
 
-echo "<script>console.log('" .$result. "');</script>";
+
+$postal_code = $result->Postal_Code;
+$guest_one_name = $result->guest_one_name;
+$guest_two_name = $result->guest_two_name;
+
+echo $guest_one_name;
+echo $postal_code;
+
+
 
 class WeddingRsvpForm  {
 
@@ -80,7 +86,14 @@ class WeddingRsvpForm  {
 
     }
 
+    public function load_variables()
+    {
+        global $guest_one_name;
+        global $guest_two_name;
+    }
+
     public function load_shortcode()
+
     {?>
         <h2>We're so excited to hear from you!</h2>
 
@@ -97,30 +110,34 @@ class WeddingRsvpForm  {
 
         </form>
 
-        <h3>Will You Attend our Event?</h3>
-        <form method="post">
-        <input type="radio" id="rsvp-yes" name="rsvp-button" value="YES" onchange="chooseToAttend()">
-        <label for="rsvp-yes">I'll Be There with Bells On!</label><br>
-        <input type="radio" id="rsvp-no" name="rsvp-button" value="NO" onchange="chooseNotToAttend()">
-        <label for="rsvp-no">I regrettably cannot attend.</label><br><br>
-        </form>
+        <label for="rsvp_progress">Your Progress:</label>
+        <progress id="rsvp_progress" value="33" max="100"> 33% </progress>
+        <br><br>
+
+        <h3>Will You, or Anyone in your Household, Attend our Event?</h3>
+        
+        <input type="radio" id="rsvp-yes" name="rsvp-button" value="YES">
+        <label for="rsvp-yes">Yes! Let's confirm!</label><br>
+        <input type="radio" id="rsvp-no" name="rsvp-button" value="NO">
+        <label for="rsvp-no">Regrettably, we cannot attend.</label><br><br>
 
         <div id="rsvp-details" class="hide">
 
         </div>
 
-        <div id="rsvp-attending" class="hide">
+        <div id="rsvp-attending" class="show">
 
-            <form>
+        <label for="rsvp_progress">Your Progress:</label>
+        <progress id="rsvp_progress" value="66" max="100"> 66% </progress>
+        <br><br>
 
-                <label for='amount'>Tell us how many people in your household will attend.</label><br>
-                <input type="number" id="amount"><br><br>
+            <form method="post">
 
                 <h3>So, who's all coming?</h3>
                 <label for='first-guest'>First Guest</label><br>
-                <input type="text" id="first-guest"><br>
+                <input type="text" id="first-guest" value="<?php echo $guest_one_name; ?>"><br>
                 <label for='second-guest'>Second Guest</label><br>
-                <input type="text" id="second-guest"><br>
+                <input type="text" id="second-guest" value="<?php echo $guest_two_name; ?>"><br>
                 <label for='third-guest'>Third Guest</label><br>
                 <input type="text" id="third-guest"><br>
                 <label for='fourth-guest'>Fourth Guest</label><br>
@@ -128,10 +145,19 @@ class WeddingRsvpForm  {
 
                 <h3>Do You Want Us to Keep You Updated?</h3>
 
-                <label for='email_one'>Main Email Address</label><br>
-                <input type="email" id="email_one"><br>
-                <label for='email_two'>Secondary Email Address</label><br>
-                <input type="email" id="email_two"><br><br><br>
+                
+                <input type="radio" id="check-no-email" name="check-email" onchange="chooseNotToAttend()">
+                <label for='check-no-email'>Nope - we're good.</label>
+                <input type="radio" id="check-yes-email" name="check-email" onchange="chooseToAttend()">
+                <label for='check-yes-email'>Let's keep in touch!</label>
+                <br><br>
+                <div id="email-attend-input-section" class="hide">
+                    <label for='email_one'>Main Email Address</label><br>
+                    <input type="email" id="email_one"><br>
+                    <label for='email_two'>Secondary Email Address</label><br>
+                    <input type="email" id="email_two"><br><br><br>
+                </div>
+
 
                 <input type="submit" value="Click to Send Us your RSVP!">
 
@@ -139,7 +165,7 @@ class WeddingRsvpForm  {
 
         </div>
 
-        <div id="rsvp-not-attending" class="hide">
+        <div id="rsvp-not-attending" class="show">
             <p>
                 We are so sorry to hear you cannot join us on our special day, but we do appreciate you letting us know!
             </p>
