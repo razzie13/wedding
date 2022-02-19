69004da4324f02875b909e7b4f9549bd1fa3682f
razzie13 <gregrasmussen@gmail.com>
2022-02-18 22:07:30 -0500

    Form Editing

--- plugins/wedding-rsvp-form/js/wedding-rsvp-form.js
+++ plugins/wedding-rsvp-form/js/wedding-rsvp-form.js
@@ -9,3 +9,82 @@ const chooseNotToAttend = () => {
         document.getElementById('email-attend-input-section').classList.remove('show');
         document.getElementById('email-attend-input-section').classList.add('hide');
 }
+
+const clickFormButtonOne = (e) => {
+
+    console.log(e);
+    document.getElementById('form-login').classList.remove('show');
+    document.getElementById('form-login').classList.add('hide');
+    document.getElementById('form-section-one').classList.remove('hide');
+    document.getElementById('form-section-one').classList.add('show');
+    document.getElementById('form-section-two').classList.remove('hide');
+    document.getElementById('form-section-two').classList.add('show');
+    document.getElementById('form-section-three').classList.remove('show');
+    document.getElementById('form-section-three').classList.add('hide');
+    document.getElementById('form-section-four').classList.remove('show');
+    document.getElementById('form-section-four').classList.add('hide');
+}
+
+const clickFormButtonTwo = (e) => {
+
+    console.log(e);
+    document.getElementById('form-login').classList.remove('show');
+    document.getElementById('form-login').classList.add('hide');
+    document.getElementById('form-section-one').classList.remove('show');
+    document.getElementById('form-section-one').classList.add('hide');
+    document.getElementById('form-section-two').classList.remove('hide');
+    document.getElementById('form-section-two').classList.add('show');
+    document.getElementById('form-section-three').classList.remove('show');
+    document.getElementById('form-section-three').classList.add('hide');
+    document.getElementById('form-section-four').classList.remove('show');
+    document.getElementById('form-section-four').classList.add('hide');
+}
+
+const clickFormButtonThree = (e) => {
+    document.getElementById('form-login').classList.remove('show');
+    document.getElementById('form-login').classList.add('hide');
+    document.getElementById('form-section-one').classList.remove('show');
+    document.getElementById('form-section-one').classList.add('hide');
+    document.getElementById('form-section-two').classList.remove('show');
+    document.getElementById('form-section-two').classList.add('hide');
+    document.getElementById('form-section-three').classList.remove('hide');
+    document.getElementById('form-section-three').classList.remove('show');
+    document.getElementById('form-section-four').classList.remove('show');
+    document.getElementById('form-section-four').classList.add('hide');
+}
+
+const clickFormBackButtonThree = (e) => {
+
+    console.log(e);
+    document.getElementById('form-login').classList.remove('show');
+    document.getElementById('form-login').classList.add('hide');
+    document.getElementById('form-section-one').classList.remove('show');
+    document.getElementById('form-section-one').classList.add('hide');
+    document.getElementById('form-section-two').classList.remove('hide');
+    document.getElementById('form-section-two').classList.add('show');
+    document.getElementById('form-section-three').classList.remove('show');
+    document.getElementById('form-section-three').classList.add('hide');
+    document.getElementById('form-section-four').classList.remove('show');
+    document.getElementById('form-section-four').classList.add('hide');
+}
+
+const clickFormBackButtonFour = (e) => {
+
+    console.log(e);
+    document.getElementById('form-login').classList.remove('show');
+    document.getElementById('form-login').classList.add('hide');
+    document.getElementById('form-section-one').classList.remove('show');
+    document.getElementById('form-section-one').classList.add('hide');
+    document.getElementById('form-section-two').classList.remove('show');
+    document.getElementById('form-section-two').classList.add('hide');
+    document.getElementById('form-section-three').classList.remove('hide');
+    document.getElementById('form-section-three').classList.add('show');
+    document.getElementById('form-section-four').classList.remove('show');
+    document.getElementById('form-section-four').classList.add('hide');
+}
+
+const validatePostalOrZipCode = () =>  {
+    if (document.getElementById('postcode') == undefined || document.getElementById('postcode').length < 5)  {
+        document.getElementById('postcode').disabled = true;
+    }
+}
--- plugins/wedding-rsvp-form/wedding-rsvp-form.php
+++ plugins/wedding-rsvp-form/wedding-rsvp-form.php
@@ -12,22 +12,40 @@
 
 require_once( $_SERVER['DOCUMENT_ROOT'] . '/wp-load.php' );
 
+// WORDPRESS GLOBAL
 global $wpdb;
 
-$searched_postcode = $_POST['postcode'];
+// DECLARE ALL VARIABLES
+$postal_code = null;
+$guest_one_name = null;
+$guest_two_name = null;
+$guest_three_name = null;
+$guest_four_name = null;
+$email_address_one = null;
+$email_address_two = null;
 
+$searched_postcode = null;
+
+if ( $_SERVER['REQUEST_METHOD'] == 'POST' ) {
+    $searched_postcode = $_POST['postcode'];
+}
+
+// QUERY MYSQL DATABASE
 $query = "SELECT * FROM invite_responses WHERE Postal_Code = '{$searched_postcode}'";
 $result = $wpdb->get_row($query);
 
+// SET VARIABLES WITH DATA FROM MYSQL IF QUERY IS SUCCESSFUL 
+if($result)  {
+    $postal_code = $result->Postal_Code;
+    $guest_one_name = $result->guest_one_name;
+    $guest_two_name = $result->guest_two_name;
+    $guest_three_name = $result->guest_three_name;
+    $guest_four_name = $result->guest_four_name;
+    $email_address_one = $result->email_address_one;
+    $email_address_two = $result->email_address_two;
+}
 
 
-$postal_code = $result->Postal_Code;
-$guest_one_name = $result->guest_one_name;
-$guest_two_name = $result->guest_two_name;
-$guest_three_name = $result->guest_three_name;
-$guest_four_name = $result->guest_four_name;
-$email_address_one = $result->email_address_one;
-$email_address_two = $result->email_address_two;
 
 class WeddingRsvpForm  {
 
@@ -88,6 +106,7 @@ class WeddingRsvpForm  {
     public function load_shortcode()
 
     {
+        // IMPORT VARIABLES
         global $postal_code;
         global $guest_one_name;
         global $guest_two_name;
@@ -95,45 +114,57 @@ class WeddingRsvpForm  {
         global $guest_four_name;
         global $email_address_one;
         global $email_address_two;
+
+        $guest_array = array($guest_two_name, $guest_three_name, $guest_four_name);
         
+        $security_codes = array('ab','ab','ab','ab','ab','ab','ab','ab','ab','ab');
+
+
         ?>
         <h2>We're so excited to hear from you!</h2>
         <br>
 
-        <form method="post">
+        <form method="post" action="">
 
-            <label for='postcode'>Enter your Postal/Zip Code:</label><br>
-            <input type="text" id="postcode" name="postcode"><br><br>
+            <div id="form-login" class="show">
+                <label for='postcode'>Enter your Postal/Zip Code:</label><br>
+                <input type="text" id="postcode" name="postcode"><br><br>
 
-            <input type="submit" value="Next" name="submitbutton">
+                <input type="submit" value="Next" name="submit-button-one"  onclick="clickFormButtonOne()">
+            </div>
 
         </form>
 
-        <label for="rsvp_progress">Your Progress:</label>
-        <progress id="rsvp_progress" value="25" max="100"> 25% </progress>
-        <br><br>
-
-        <h3>Will You, or Anyone in your Household, Attend our Event?</h3>
         
-        <input type="radio" id="rsvp-yes" name="rsvp-button" value="YES">
-        <label for="rsvp-yes">Yes! Let's confirm!</label><br>
-        <input type="radio" id="rsvp-no" name="rsvp-button" value="NO">
-        <label for="rsvp-no">Regrettably, we cannot attend.</label><br><br>
 
-        <div id="rsvp-details" class="hide">
+        <form method="post" action="">
 
-        </div>
+            <div id="form-section-one" class="hide">
 
-        <div id="rsvp-attending" class="show">
+                <label for="rsvp_progress">Your Progress:</label>
+                <progress id="rsvp_progress" value="25" max="100"> 25% </progress>
+                <br><br>
 
-        <label for="rsvp_progress">Your Progress:</label>
-        <progress id="rsvp_progress" value="50" max="100"> 50% </progress>
-        <br><br>
+                <h3>Will You, or Anyone in your Household, Attend our Event?</h3>
+        
+                <input type="radio" id="rsvp-yes" name="rsvp-button" value="YES">
+                <label for="rsvp-yes">Yes! Let's confirm!</label><br>
+                <input type="radio" id="rsvp-no" name="rsvp-button" value="NO">
+                <label for="rsvp-no">Regrettably, we cannot attend.</label><br><br>
 
-            <form method="post">
+                <input type="button" value="Next" name="submit-button-two" onclick="clickFormButtonTwo(value)"></input>
+
+            </div>
+
+            <div id="form-section-two" class="hide">
+
+                <label for="rsvp_progress">Your Progress:</label>
+                <progress id="rsvp_progress" value="50" max="100"> 50% </progress>
+                <br><br>
 
                 <h3>So, who's all coming?</h3>
-                <h4 class="header-four-rsvp">You May Edit or Remove Names from the Guestlist Here</h4>
+
+                <h4 class="header-four-rsvp">You May Confirm <?php if (in_array('Guest', $guest_array))  {echo 'or Edit ';} ?>Names from the Guestlist Here</h4>
                 <label for='first-guest'>First Guest</label><br>
                 <input type="text" id="first-guest" value="<?php echo $guest_one_name; ?>"><br>
                 <?php if ($guest_two_name != null)  {
@@ -148,11 +179,11 @@ class WeddingRsvpForm  {
                     echo "<label for='second-guest'>Fourth Guest</label><br>";
                     echo "<input type='text' id='fourth-guest' value='$guest_four_name'><br>";
                 } ?>
-                
+            
                 <h3>Do You Want Us to Keep You Updated?</h3>
                 <h4 class="header-four-rsvp">Add or Edit Your Email Address Here</h4>
 
-                
+            
                 <input type="radio" id="check-no-email" name="check-email" onchange="chooseNotToAttend()">
                 <label for='check-no-email'>Nope - we're good.</label>
                 <input type="radio" id="check-yes-email" name="check-email" onchange="chooseToAttend()">
@@ -165,6 +196,13 @@ class WeddingRsvpForm  {
                     <input type="email" id="email_two" placeholder="example_two@example.com"><br><br><br>
                 </div>
 
+                <input type="button" value="Prev" name="back-button-three" onclick="clickFormBackButtonThree()"></input>
+                <input type="button" value="Next" name="submit-button-three" onclick="clickFormButtonThree()"></input>
+
+            </div>
+
+            <div id="form-section-three" class="hide">
+
                 <label for="rsvp_progress">Your Progress:</label>
                 <progress id="rsvp_progress" value="75" max="100"> 75% </progress>
                 <br><br>
@@ -172,13 +210,16 @@ class WeddingRsvpForm  {
                 <h3>Would you like to leave us a note?</h3>
                 <textarea></textarea>
 
-                <input type="submit" value="Click to Send Us your RSVP!">
+                <input type="button" value="Prev" name="back-button-three" onclick="clickFormBackButtonFour()"></input>
+                <input type="submit" value="Next" name="submit-button-four">Click Here to Send Us Your RSVP</inp>
 
-            </form>
+            </div>
+
+        </form>
 
         </div>
 
-        <div id="rsvp-not-attending" class="show">
+        <div id="rsvp-not-attending" class="hide">
             <p>
                 We are so sorry to hear you cannot join us on our special day, but we do appreciate you letting us know!
             </p>
