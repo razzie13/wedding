a0d4c1c25bd54232a90aa4c54f9a72249de33e6e
razzie13 <gregrasmussen@gmail.com>
2022-02-28 23:43:31 -0500

    Database Updating

--- plugins/wedding-rsvp-form/includes/inviteaccept.inc.php
+++ plugins/wedding-rsvp-form/includes/inviteaccept.inc.php
@@ -1,8 +1,8 @@
 <?php
 require_once( $_SERVER['DOCUMENT_ROOT'] . '/wp-load.php' );
 
-// WORDPRESS GLOBAL
-//global $wpdb;
+
+
 
 // VARIABLES
 
@@ -10,8 +10,8 @@ $postal_code = $_POST['accept-postal-code'];
 
 $guest_one_name = $_POST['first-guest'];
 $guest_two_name = $_POST['second-guest'];
-//$guest_three_name = $_POST['third-guest'];
-//$guest_four_name = $_POST['fourth-guest'];
+$guest_three_name = $_POST['third-guest'];
+$guest_four_name = $_POST['fourth-guest'];
 
 //$guest_one_status = $_POST['first-guest-attending'];
 //$guest_two_status = $_POST['second-guest-attending'];
@@ -23,24 +23,38 @@ $email_address_two = $_POST['email-address-two'];
 
 $user_comments = $_POST['user-comments'];
 
+// WORDPRESS GLOBAL
 
+global $wpdb;
+
+// VARIABLE DECLARATIONS
+
+// global $guest_one_name;
+// global $guest_two_name;
+// global $guest_three_name;
+// global $guest_four_name;
+
+// global $email_address_one;
+// global $email_address_two;
+
+// global $user_comments;
 
 $wpdb->update(
         'invite_responses', 
         array( 
         'logged_in_once' => 'TRUE',
-        'household_attending' => 'TRUE'
-        'guest_one_name' => "'$guest_one_name'",
+        'household_attending' => 'TRUE',
+        'guest_one_name' => $guest_one_name,
         //'guest_one_coming' => $guest_one_status,
-        'guest_two_name' => "'$guest_two_name'",
+        'guest_two_name' => $guest_two_name,
         //'guest_two_coming' => $guest_two_status,
-        'guest_three_name' => "'$guest_three_name'",
+        'guest_three_name' => $guest_three_name,
         //'guest_three_coming' => $guest_three_status,
-        'guest_four_name' => "'$guest_four_name'",
+        'guest_four_name' => $guest_four_name,
         //'guest_four_status' => $guest_four_status,
-        'email_address_one' => "'$email_address_one'",
-        //'email_address_two' => "'$email_address_two'",
-        'user_comments' => "'$user_comments'"
+        'email_address_one' => $email_address_one,
+        'email_address_two' => $email_address_two,
+        'user_comments' => $user_comments
         ),
         array(
         'Postal_Code' => $postal_code
@@ -48,7 +62,7 @@ $wpdb->update(
 );
 
 $searched_postcode = $postal_code;
+
+var_dump($_POST['accept-postal-code']);
 var_dump($_POST);
-//echo $guest_one_name;
-//echo $guest_two_name;
 //header("Location: http://gregandnicki.com/rsvp?reply=success");
