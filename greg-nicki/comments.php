c98b40f77299aa20a2092a26e16c3cdbb4faa54b
razzie13 <gregrasmussen@gmail.com>
2022-03-16 23:53:45 -0400

    Gift Registry Comment Section Conversion

--- greg-nicki/comments.php
+++ greg-nicki/comments.php
@@ -30,34 +30,28 @@ if ( post_password_required() ) {
 			<?php
 			$greg_nicki_comment_count = get_comments_number();
 			if ( '1' === $greg_nicki_comment_count ) {
-				printf(
-					/* translators: 1: title. */
-					esc_html__( 'One thought on &ldquo;%1$s&rdquo;', 'greg-nicki' ),
-					'<span>' . wp_kses_post( get_the_title() ) . '</span>'
-				);
-			} else {
-				printf( 
-					/* translators: 1: comment count number, 2: title. */
-					esc_html( _nx( '%1$s thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', $greg_nicki_comment_count, 'comments title', 'greg-nicki' ) ),
-					number_format_i18n( $greg_nicki_comment_count ), // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-					'<span>' . wp_kses_post( get_the_title() ) . '</span>'
-				);
-			}
+				echo 'This item has been purchased. Thank you for your interest!';
+				// printf(
+				// 	/* translators: 1: title. */
+					
+				// 	esc_html__( 'One thought on &ldquo;%1$s&rdquo;', 'greg-nicki' ),
+				// 	'<span>' . wp_kses_post( get_the_title() ) . '</span>'
+				// );
+			} 
+			// else {
+			// 	printf( 
+			// 		/* translators: 1: comment count number, 2: title. */
+			// 		esc_html( _nx( '%1$s thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', $greg_nicki_comment_count, 'comments title', 'greg-nicki' ) ),
+			// 		number_format_i18n( $greg_nicki_comment_count ), // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+			// 		'<span>' . wp_kses_post( get_the_title() ) . '</span>'
+			// 	);
+			// }
 			?>
 		</h2><!-- .comments-title -->
 
 		<?php the_comments_navigation(); ?>
 
-		<ol class="comment-list">
-			<?php
-			wp_list_comments(
-				array(
-					'style'      => 'ol',
-					'short_ping' => true,
-				)
-			);
-			?>
-		</ol><!-- .comment-list -->
+
 
 		<?php
 		the_comments_navigation();
@@ -71,7 +65,14 @@ if ( post_password_required() ) {
 
 	endif; // Check for have_comments().
 
-	comment_form();
+	$greg_nicki_comment_count = get_comments_number();
+			if ( '1' === $greg_nicki_comment_count ) {
+				echo '';
+			} else  {
+				comment_form();
+			}
+
+	
 	?>
 
 </div><!-- #comments -->
